7d9f23366fe5f8c5b62a0757206af5a51ae89e88
Adam <adamjcoonen@gmail.com>
2020-10-16 18:06:28 -0400

    Updates

---
+++ precache-manifest.34fe23f9291219b65251f0d61d3a9499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d170e046e0281964532db76b6c236ffb",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "88456d539e091ad2ac5a",
+    "url": "/project-4-assessment/static/css/main.8b9b3772.chunk.css"
+  },
+  {
+    "revision": "5947ca62461378555b52",
+    "url": "/project-4-assessment/static/js/2.e9e0653a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.e9e0653a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88456d539e091ad2ac5a",
+    "url": "/project-4-assessment/static/js/main.ee30809e.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
